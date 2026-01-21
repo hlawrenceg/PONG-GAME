0b4d4f3d4d71e1d83b17d14b48306a94ce30b0ed
hlawrenceg <hagaa019@osloskolen.no>
2026-01-21 09:30:07 +0100

    drew cpu, player and ball

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ pg.init()
 
 WIDTH, HEIGHT = 800, 600
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-screen.fill(("white"))
+screen.fill(("black"))
 
+ball = pg.Rect(0, 0, 30, 30)
+ball.center = (WIDTH // 2, HEIGHT // 2)
+
+cpu = pg.Rect(0, 0, 20, 100)
+cpu.centery = HEIGHT // 2
+
+player = pg.Rect(0, 0, 20, 100)
 running = True
+
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+       
+        pg.draw.ellipse(screen,'white', ball)
+        pg.draw.rect(screen, 'white', cpu)
+        pg.draw.rect(screen, 'white', player)
+
         pg.display.update()
-    
