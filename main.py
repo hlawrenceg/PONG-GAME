d0b0d46f610b6f11e5955e4d9abc9024f72d0843
hlawrenceg <hagaa019@osloskolen.no>
2026-01-21 08:59:34 +0100

    ferdig screen

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pygame as pg
 pg.init()
 
 WIDTH, HEIGHT = 800, 600
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+screen.fill(("white"))
 
 running = True
-
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        pg.display.update()
+    
